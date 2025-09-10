3c6737fe4c001d68206e3d86c712b3fc8f697d1c
Mark J. Berger <git@markjberger.com>
2025-09-10 08:06:03 -0700

    clean up ClientRequest

--- internal/api/request.go
+++ internal/api/request.go
@@ -11,24 +11,22 @@ import (
 // a wrapper struct around kmsg.Request since franz-go does not have
 // a reason to include this representation.
 type ClientRequest struct {
-	key           int16
-	version       int16
+	request       kmsg.Request
 	correlationID int32
 	clientID      string
 	tags          kmsg.Tags
-	request       kmsg.Request
 }
 
-func (r *ClientRequest) Key() int16           { return r.key }
-func (r *ClientRequest) KeyName() string      { return kmsg.NameForKey(r.key) }
-func (r *ClientRequest) Version() int16       { return r.version }
+func (r *ClientRequest) Key() int16           { return r.request.Key() }
+func (r *ClientRequest) KeyName() string      { return kmsg.NameForKey(r.Key()) }
+func (r *ClientRequest) Version() int16       { return r.request.GetVersion() }
 func (r *ClientRequest) CorrelationID() int32 { return r.correlationID }
 func (r *ClientRequest) ClientID() string     { return r.clientID }
 func (r *ClientRequest) Body() kmsg.Request   { return r.request }
 
 func (r *ClientRequest) ReadFrom(b *kbin.Reader) error {
-	r.key = b.Int16()
-	r.version = b.Int16()
+	key := b.Int16()
+	version := b.Int16()
 	r.correlationID = b.Int32()
 	s := b.NullableString()
 	if s != nil {
@@ -37,11 +35,11 @@ func (r *ClientRequest) ReadFrom(b *kbin.Reader) error {
 
 	// Determine whether request is "flexible" i.e. supports _tagged_fields
 	// https://kafka.apache.org/protocol#protocol_messages
-	request := kmsg.RequestForKey(r.key)
+	request := kmsg.RequestForKey(key)
 	if request == nil {
-		return fmt.Errorf("api key has no associated request: %d", r.key)
+		return fmt.Errorf("api key has no associated request: %d", key)
 	}
-	request.SetVersion(r.version)
+	request.SetVersion(version)
 	if request.IsFlexible() {
 		r.tags = kmsg.ReadTags(b)
 	}
